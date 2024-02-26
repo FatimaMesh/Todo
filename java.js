f1af04b4304918ad08c97cbcbd0da9ef7c1cec38
FatimaMesh <fatimah1011f@gmail.com>
2024-02-26 17:18:17 +0300

    First draft todo

--- java.js
+++ java.js
@@ -1,6 +1,5 @@
 const todoContainer = document.getElementsByTagName("main")[0];
-// const todoCard = document.getElementsByTagName("input");
-const timeContainer = document.getElementById('digital_clock');
+const timeContainer = document.getElementById("digital_clock");
 let todo = [];
 
 setInterval(digitalClock, 1000);
@@ -11,27 +10,31 @@ function digitalClock() {
 }
 
 function CreateTodoContainer() {
-    for(let list of todo){
-  let div = document.createElement("div");
-  let { id, title, completed } = list;
-  let checked = "";
-  if (completed){
-    checked = 'checked';
-  }
-    div.innerHTML = `<h2 class="card_id">${id}</h2><label for="list"></label> <input type="checkbox" name="list" id="list" ${checked}/><p class='${checked} card_title'>${title}</p>`;
-        div.innerHTML = `<h2 class="card_id">${id}</h2><label for="list"></label> <input type="checkbox" name="list" id="list" ${checked}/><p class='${checked} card_title'>${title}</p>`;
-  todoContainer.appendChild(div);
-  div.classList = "card";
+  for (let list of todo) {
+    let div = document.createElement("div");
+    let { id, title, completed } = list;
+    let checked = "";
+    if (completed) {
+      checked = "checked";
     }
+    div.innerHTML = `<h2 class="card_id">${id}</h2><label for="list"></label> <input type="checkbox" name="list" id="list" ${checked}/><p class='${checked} card_title'>${title}</p>`;
+    todoContainer.appendChild(div);
+    div.classList = "card";
+  }
 }
 
 const TodoListFetch = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => response.json())
-    .then((data) => {
-        todo = data;
+    .then((response) => {
+      if (!response.ok) {
+        throw "There is something wrong ..!";
+      }
+      return response.json();
     })
-    .then(()=> CreateTodoContainer())
+    .then((data) => {
+      todo = data;
+    })
+    .then(() => CreateTodoContainer())
     .catch((error) => console.error("Error:", error));
 };
 
