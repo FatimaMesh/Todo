2b53d2c248fc970704ea6e1521f2fea9bc34f62e
FatimaMesh <fatimah1011f@gmail.com>
2024-02-26 15:57:02 +0300

    end  page todo

--- java.js
+++ java.js
@@ -1,5 +1,14 @@
 const todoContainer = document.getElementsByTagName("main")[0];
+// const todoCard = document.getElementsByTagName("input");
+const timeContainer = document.getElementById('digital_clock');
 let todo = [];
+
+setInterval(digitalClock, 1000);
+
+function digitalClock() {
+  const date = new Date();
+  timeContainer.textContent = date.toLocaleTimeString();
+}
 
 function CreateTodoContainer() {
     for(let list of todo){
@@ -9,11 +18,13 @@ function CreateTodoContainer() {
   if (completed){
     checked = 'checked';
   }
-    div.innerHTML = `<h2 class="card_id">${id}</h2> <input type="checkbox" name="list" ${checked}/><p>${title}</p>`;
+    div.innerHTML = `<h2 class="card_id">${id}</h2><label for="list"></label> <input type="checkbox" name="list" id="list" ${checked}/><p class='${checked} card_title'>${title}</p>`;
+        div.innerHTML = `<h2 class="card_id">${id}</h2><label for="list"></label> <input type="checkbox" name="list" id="list" ${checked}/><p class='${checked} card_title'>${title}</p>`;
   todoContainer.appendChild(div);
   div.classList = "card";
     }
 }
+
 const TodoListFetch = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
     .then((response) => response.json())
